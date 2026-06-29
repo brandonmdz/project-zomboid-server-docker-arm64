4a0de814bf3d900edc3a70cc3b03ca0e046c39e2
Brandon Zalazar <113209323+brandonmdz@users.noreply.github.com>
2026-06-29 20:00:59 -0300

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-set -e
+export JAVA_TOOL_OPTIONS="-Xint"
 
-echo "Updating Project Zomboid server..."
+FEXBash "/home/steam/Steam/steamcmd.sh +force_install_dir /home/steam/pzserver +login anonymous +app_update 380870 validate +quit"
 
-FEXBash "/home/steam/Steam/steamcmd.sh +force_install_dir /home/steam/pzserver +login anonymous +app_update 380870 +quit"
+screen -d -m -S zomboid /bin/bash -c ' \
+  while true; do \
+    FEXBash "/home/steam/pzserver/start-server.sh -servername ${SERVERNAME}"; \
+    echo "The server will restart in 10 seconds. If you want to stop the server, press Ctrl+C."; \
+    for i in 10 9 8 7 6 5 4 3 2 1; do echo "$i..."; sleep 1; done \
+  done \
+'
 
-echo "Starting Project Zomboid server..."
-
-exec FEXBash "/home/steam/pzserver/start-server.sh -servername ${SERVERNAME}"
+sleep infinity
